1a5dea360d6b56530f923ca0ed738a59c5cc636b
neelapala6i6i <chinnumuthyalu@gmail.com>
2026-09-15 21:40:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Sandwiched Vowels/remove-sandwiched-vowels.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String sandwichedVowel(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (i > 0 && i < s.length() - 1 &&
+                "aeiou".indexOf(s.charAt(i)) != -1 &&
+                "aeiou".indexOf(s.charAt(i - 1)) == -1 &&
+                "aeiou".indexOf(s.charAt(i + 1)) == -1) {
+
+                continue;
+            }
+
+            sb.append(s.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
